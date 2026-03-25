31a4c16f77d7f608dc1dfc7df9c21fe7dcac6b2a
Sahil Kulria <sahilkulria002@gmail.com>
2026-03-25 10:29:27 +0530

    fgfgf

---
+++ docs/programming/common/common-code.js
@@ -0,0 +1,19 @@
+// Common programming utilities and shared code snippets
+// This file contains reusable functions and constants for programming tutorials
+
+// Example: Utility function for logging
+function logMessage(message) {
+    console.log(`[Programming Hub] ${message}`);
+}
+
+// Example: Common code snippets object
+const commonSnippets = {
+    python: {
+        helloWorld: 'print("Hello, World!")',
+        loop: 'for i in range(5):\n    print(i)'
+    },
+    arduino: {
+        setup: 'void setup() {\n    // put your setup code here, to run once:\n}',
+        loop: 'void loop() {\n    // put your main code here, to run repeatedly:\n}'
+    }
+};
