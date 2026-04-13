d78e4731e24474b66008710bc612643faa7aabb7
Sahil Kulria <sahilkulria002@gmail.com>
2026-04-13 17:24:36 +0530

    new time code

--- docs/programming/arduino/if-else-levels/level-14-if-else-rtc-ds3231-led.ino
+++ docs/programming/arduino/if-else-levels/level-14-if-else-rtc-ds3231-led.ino
@@ -2,21 +2,36 @@
 #include <RTClib.h>
 
 RTC_DS3231 rtc;
-const int ledPin = 13;
 
 void setup() {
-    pinMode(ledPin, OUTPUT);
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    if (!rtc.begin()) {
-        Serial.println("RTC not found");
-        while (1) {}
-    }
+  if (!rtc.begin()) {
+    Serial.println("RTC not found");
+    while (1);
+  }
+
+  // Only set time if RTC lost power
+  // if (rtc.lostPower()) {
+    // Serial.println("RTC lost power, setting time...");
+    
+    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // } else {
+    // Serial.println("RTC already has correct time");
+  // }
 }
 
 void loop() {
-    DateTime now = rtc.now();
-    int sec = now.second();
+  DateTime now = rtc.now();
+
+  Serial.print("Time: ");
+  Serial.print(now.hour());
+  Serial.print(":");
+  Serial.print(now.minute());
+  Serial.print(":");
+  Serial.println(now.second());
+
+   int sec = now.second();
 
     if (sec % 3 == 0) {
         digitalWrite(ledPin, HIGH);
@@ -24,6 +39,6 @@ void loop() {
         digitalWrite(ledPin, LOW);
     }
 
-    Serial.println(sec);
-    delay(200);
+
+  delay(500);
 }
